32737aa7babaa883326ba853d0fcca0ebc393cca
eyezLee <lmt18072747669@gmail.com>
2018-12-23 15:41:52 +0800

    correct vertices generation

--- Assets/scripts/Matrix.cs
+++ Assets/scripts/Matrix.cs
@@ -6,28 +6,66 @@ using UnityEngine;
 
 public class Matrix : MonoBehaviour
 {
-    int numInWidth = 10;
-    int numInHeight = 10;
+    int voxelInWidth = 4;
+    int voxelInHeight = 4;
+    float width = 4.0f;
+    float height = 4.0f;
 
-    float gap = 0.45f;
-    float size = 0.15f;
+    float size = 0.2f;
     Mesh mesh;
+    Vector3[] vertices;
 
-    void Generator()
+	private void Start()
+	{
+        Generator();
+	}
+    //vertices test
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.black;
+        if (vertices == null)
+        {
+            return;
+        }
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+	}
+
+	void Generator()
     {
         //create mesh object
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-        float stripe = gap + size;
+
+        float stripe = width / voxelInWidth;
+        int vertexInWidth = voxelInWidth * 2;
+        int vertexInHeight = voxelInHeight * 2;
 
         //generate vertices
-        int verticesNum = numInWidth * 2 * numInHeight * 2;
-        Vector3[] vertices = new Vector3[verticesNum];
+        vertices = new Vector3[vertexInWidth * vertexInHeight];
         int index = 0;
-        for (int x = 0; x < numInWidth * 2; x++)
+        for (int y = 0; y < vertexInHeight; y++)
         {
-            for (int y = 0; y < numInHeight * 2; y++)
+            for (int x = 0; x < vertexInWidth; x++)
             {
-                vertices[index] = new Vector3(x * stripe / 2, y * stripe / 2, 0);
+                if(x % 2 == 0 && y % 2 == 0)
+                {
+                    vertices[index] = new Vector3(x * stripe, y * stripe);
+                }
+                if(x % 2 == 1 && y % 2 == 0)
+                {
+                    vertices[index] = new Vector3((x - 1) * stripe + size, y * stripe);
+                }
+                if(x % 2 == 0 && y % 2 == 1)
+                {
+                    vertices[index] = new Vector3(x * stripe, (y - 1) * stripe + size);
+                }
+                if(x % 2 ==1 && y % 2 == 1)
+                {
+                    vertices[index] = new Vector3((x - 1) * stripe + size, (y - 1) * stripe + size);
+                }
+                index += 1;
             }
         }
         mesh.vertices = vertices;
@@ -35,5 +73,20 @@ public class Matrix : MonoBehaviour
         //generate uv
 
         //draw triangles
+        int[] triangles = new int[voxelInWidth * voxelInHeight * 6 * 2];
+        int ti = 0, vi = 0;
+        for (int y = 0; y < voxelInHeight * 1; y++)
+        {
+            for (int x = 0; x < voxelInWidth * 1; x++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 1] = triangles[ti + 3] = vi + 1;
+                triangles[ti + 2] = triangles[ti + 4] = voxelInWidth * 2 + vi + 1;
+                triangles[ti + 5] = voxelInWidth * 2 + vi + 2;
+                ti += 6;
+                vi += 2;
+            }
+        }
+       // mesh.triangles = triangles;
     }
 }
